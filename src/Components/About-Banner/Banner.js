eff76c2e8b821aaf5c89b6706d03aed2022e9ea2
Mpumelelo Kgaugelo Mpe <53508326+kgauzampe@users.noreply.github.com>
2023-08-09 16:48:28 +0200

    Btn added

--- src/Components/About-Banner/Banner.js
+++ src/Components/About-Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Typography } from "@mui/material";
 import AboutImg from '../../Images/Dr Overton.png'
 import { Grid } from '@mui/material';
+import Button from '@mui/material/Button';
 import './Banner.css';
 
 const Banner = () => {
@@ -9,7 +10,7 @@ const Banner = () => {
     <div className="banner">
       <div className="banner-content">
         <h2>About Us</h2>
-        <Grid container spacing={10}>
+        <Grid container spacing={6}>
           <Grid item xs={12} sm={12} md={6}>
             <Typography>
               Welcome to our IT consulting and training company! We empower businesses with comprehensive
@@ -23,6 +24,9 @@ const Banner = () => {
               Welcome to our IT consulting and training company! We empower businesses with comprehensive solutions and training
               programs to thrive in the digital age.
             </Typography>
+            <Button variant="contained" size="medium" className="bannerBtn">
+              Contact Us
+            </Button>
           </Grid>
           <Grid item xs={12} sm={12} md={6}>
             <img src={AboutImg} alt="Card" className="aboutimg" />
