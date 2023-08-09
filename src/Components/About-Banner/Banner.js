2808812c5492c86470c3a44c5bf86b2531cf31b7
Mpumelelo Kgaugelo Mpe <53508326+kgauzampe@users.noreply.github.com>
2023-08-09 17:21:48 +0200

    Style update

--- src/Components/About-Banner/Banner.js
+++ src/Components/About-Banner/Banner.js
@@ -14,16 +14,29 @@ const Banner = () => {
         <Grid container spacing={6}>
           <Grid item xs={12} sm={12} md={6}>
             <Typography>
-              Welcome to our IT consulting and training company! We empower businesses with comprehensive
-              solutions and training programs to thrive in the digital age. Our experienced team offers tailored
-              consulting services, including IT strategy, infrastructure design, software development, and cybersecurity.
-              Our training courses cover programming, cloud computing, data analytics, and more. Partner with us for exceptional
-              results and personalized support. Contact us today to harness the power of technology for your success.
+              EconoTeque specialises in delivering rare Information Technology expertise and training
+              to businesses and individuals across Africa and beyond. Founded and spearheaded by Dr.
+              James Overton, a distinguished IT visionary with extensive hands-on experience in major
+              organisations like IBM and the US Air Force.
             </Typography>
             <br />
             <Typography>
-              Welcome to our IT consulting and training company! We empower businesses with comprehensive solutions and training
-              programs to thrive in the digital age.
+              Dr. Overton isn't just a leader; he's an IT trailblazer holding a PhD in Computer Science.
+              His remarkable achievements include a remarkable $6.2 million cost-saving feat for the US
+              Air Force as Lead Integration Engineer. He also led the Computer Information Systems
+              Bachelors Program at Remington College, USA, and authored three comprehensive computer
+              science textbooks to simplify the subject for students.
+            </Typography>
+            <br />
+            <Typography>
+              At EconoTeque, we prioritise practical learning. Our training programs, workshops,
+              and consultancy services are meticulously crafted to empower you and your business
+              in the digital era. Whether you're a business aiming to elevate your IT capabilities
+              or an individual navigating the tech landscape, EconoTeque is here to guide you.
+            </Typography>
+            <br />
+            <Typography>
+              Come join us, and together, let's unleash the full potential of IT in your business.
             </Typography>
             <Button variant="contained" size="medium" className="bannerBtn">
               Contact Us
