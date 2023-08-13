5cc97b739685b0f8ead730f286cdfe74ccf9ac77
Mpumelelo Kgaugelo Mpe <53508326+kgauzampe@users.noreply.github.com>
2023-08-13 16:51:20 +0200

    Update footer.js

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -13,11 +13,11 @@ const Footer = () => {
       <a href="/services">Services</a>
       <a href="/contact">Contact</a>
     </div>
-    <div className="footer-social">
+    {/* <div className="footer-social">
       <a href="https://facebook.com">Facebook</a>
       <a href="https://twitter.com">Twitter</a>
       <a href="https://instagram.com">Instagram</a>
-    </div>
+    </div> */}
     <p className='foot'>&copy; 2023 EconoTeque Powered By Kreyative Kreek X BlvckKing</p>
   </div>
       {/* <div className="footer-content">
