087ad2b6ea7d5d9d351cbd7c84178b017042763d
Mpumelelo Kgaugelo Mpe <53508326+kgauzampe@users.noreply.github.com>
2023-08-13 19:40:35 +0200

    changes

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -10,45 +10,52 @@ const Contact = () => {
     const [selectedValue, setSelectedValue] = useState('option1');
 
     const handleChange = (event) => {
-      setSelectedValue(event.target.value);
+        setSelectedValue(event.target.value);
     };
     return (
         <div className="contact">
             <Navbar />
             <div className="contact-con">
-                <Typography>Name</Typography>
-                <TextField label="Username" variant="outlined" />
-                <Typography>Surname</Typography>
-                <TextField label="Username" variant="outlined" />
-                <Typography>Email Address</Typography>
-                <TextField label="Username" variant="outlined" />
-                <Typography>Contact Number</Typography>
-                <TextField label="Username" variant="outlined" />
-                <Typography>Country</Typography>
-                <TextField label="Username" variant="outlined" />
-                <Typography>City/Town</Typography>
-                <TextField label="Username" variant="outlined" />
+                <Typography id="filled-basic" InputProps={{
+                    style: {
+                        borderRadius: "10px",
+                    }
+                }}>Name</Typography>
+                <TextField label="Name" variant="outlined" />
+                <Typography id="filled-basic">Surname</Typography>
+                <TextField label="Surname" variant="outlined" />
+                <Typography id="filled-basic">Email Address</Typography>
+                <TextField label="Email" variant="outlined" />
+                <Typography id="filled-basic">Contact Number</Typography>
+                <TextField label="Contact Number" variant="outlined" />
+                <Typography id="filled-basic">Country</Typography>
+                <TextField label="Country" variant="outlined" />
+                <Typography id="filled-basic">City/Town</Typography>
+                <TextField label="City/Town" variant="outlined" />
                 <div className="radio">
-                <FormControl component="fieldset">
-                    <Typography>
-                        Reason for contact
-                    </Typography>
-                    <RadioGroup
-                        aria-label="radio-group"
-                        name="radio-group"
-                        value={selectedValue}
-                        onChange={handleChange}
-                    >
-                        <FormControlLabel value="option1" control={<Radio />} label="Option 1" />
-                        <FormControlLabel value="option2" control={<Radio />} label="Option 2" />
-                        <FormControlLabel value="option3" control={<Radio />} label="Option 3" />
-                    </RadioGroup>
-                </FormControl>
+                    <FormControl component="fieldset">
+                        <Typography>
+                            Reason for contact
+                        </Typography>
+                        <RadioGroup
+                            aria-label="radio-group"
+                            name="radio-group"
+                            value={selectedValue}
+                            onChange={handleChange}
+                        >
+                            <FormControlLabel value="option1" control={<Radio />} label="IT Consulting" />
+                            <FormControlLabel value="option2" control={<Radio />} label="IT Training" />
+                            <FormControlLabel value="option3" control={<Radio />} label="Other" />
+                        </RadioGroup>
+                    </FormControl>
                 </div>
-                <Typography>Message</Typography>
-                <TextField label="Username" variant="outlined" />
+                <Typography id="filled-basic">Message</Typography>
+                <TextField variant="outlined" />
             </div>
-            <Footer />
+            <div className="footer-con">
+                <Footer />
+            </div>
+
         </div>
     )
 
