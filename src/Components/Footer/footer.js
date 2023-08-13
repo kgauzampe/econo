bd6a6c25b3dddac5b21c761013bbf654f579a24f
Mpumelelo Kgaugelo Mpe <53508326+kgauzampe@users.noreply.github.com>
2023-08-13 16:48:58 +0200

    Footer update

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -5,11 +5,25 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
+    <footer className="footer">    
+    <div className="footer-content">
+    <div className="footer-links">
+      <a href="/">Home</a>
+      <a href="/about">About Us</a>
+      <a href="/services">Services</a>
+      <a href="/contact">Contact</a>
+    </div>
+    <div className="footer-social">
+      <a href="https://facebook.com">Facebook</a>
+      <a href="https://twitter.com">Twitter</a>
+      <a href="https://instagram.com">Instagram</a>
+    </div>
+    <p className='foot'>&copy; 2023 EconoTeque Powered By Kreyative Kreek X BlvckKing</p>
+  </div>
+      {/* <div className="footer-content">
         <p>SERVICE   ABOUT    RESOURCES   CONTACT</p>
-        <p className='foot'>&copy; 2023 EconoTeque Powered By Kreyative Kreek X BlvckKing</p>
-      </div>
+        
+      </div> */}
     </footer>
   );
 };
