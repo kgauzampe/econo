77f05c7ca4ab62c1a88a19e6d4f67fe96c427eb9
Mpumelelo Kgaugelo Mpe <53508326+kgauzampe@users.noreply.github.com>
2023-08-28 08:15:18 +0200

    Updating Nav

--- src/Components/Navigation/Navigation/Nav.js
+++ src/Components/Navigation/Navigation/Nav.js
@@ -1,45 +1,126 @@
-// Navbar.js
+// import React, { useState, useEffect, useRef } from "react";
+// import { Link } from 'react-router-dom';
+// import { Link as ScrollLink } from 'react-scroll';
+// import "./Nav.css";
+// import logo from "../../../Images/logox.png"
 
-import React, { useState } from "react";
+// const Navbar = () => {
+//   const [isNavOpen, setIsNavOpen] = useState(false);
+
+//   const toggleNav = () => {
+//     console.log("pressed")
+//     let menu = document.getElementsByClassName("nav-items")
+//     console.log("here", menu)
+//     setIsNavOpen(!isNavOpen);
+
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <div className="nav-brand">
+//         <img src={logo} alt="Logo" className="logo" />
+//       </div>
+//       <div className="nav-items">
+//         <div className="links">
+//           <Link to="/">Home</Link>
+//           <ScrollLink to="abtBanner" smooth={true} duration={500}>
+//             About
+//           </ScrollLink>
+//           <div className="dropdown">
+//             <span className="dropdown-toggle">Services</span>
+//             <div className="dropdown-content">
+//               <Link to="/service1">Software QA Training</Link>
+//               <Link to="/service2">IT Training</Link>
+//               <Link to="/service3">IT consultancy</Link>
+//               <Link to="/service4">Software QA consultancy</Link>
+//             </div>
+//           </div>
+//           <Link to="/Contact">Contact</Link>
+//         </div>
+//       </div>
+//       <div className="nav-toggle" onClick={toggleNav}>
+//         <div className={`burger ${isNavOpen ? "toggle" : ""}`} />
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
-import "./Nav.css";
 import logo from "../../../Images/logox.png"
+import './Nav.css';
 
 const Navbar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  const isMobile = useMediaQuery('(max-width:768px)');
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
+    setDrawerOpen(open);
   };
 
   return (
-    <nav className="navbar">
-      <div className="nav-brand">
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
-      <div className="nav-items">
-        <div className="links">
-          <Link to="/">Home</Link>
-          <ScrollLink to="abtBanner" smooth={true} duration={500}>
-            About
-          </ScrollLink>
-          <div className="dropdown">
-            <span className="dropdown-toggle">Services</span>
-            <div className="dropdown-content">
-              <Link to="/service1">Software QA Training</Link>
-              <Link to="/service2">IT Training</Link>
-              <Link to="/service3">IT consultancy</Link>
-              <Link to="/service4">Software QA consultancy</Link>
-            </div>
+    <div>
+      <AppBar
+        className="navbar"
+        position="static"
+        style={{ backgroundColor: '#FFFFFF' }}>
+        <Toolbar>
+          {isMobile && (
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+          <div className="nav-brand">
+            <img src={logo} alt="Logo" className="logo" />
           </div>
-          <Link to="/Contact">Contact</Link>
+          <div className="links">
+            <Link to="/">Home</Link>
+            <ScrollLink to="abtBanner" smooth={true} duration={500}>
+              About
+            </ScrollLink>
+            <div className="dropdown">
+              <span className="dropdown-toggle">Services</span>
+              <div className="dropdown-content">
+                <Link to="/service1">Software QA Training</Link>
+                <Link to="/service2">IT Training</Link>
+                <Link to="/service3">IT consultancy</Link>
+                <Link to="/service4">Software QA consultancy</Link>             </div>
+            </div>
+            <Link to="/Contact">Contact</Link>
+          </div>
+          {/* Additional Navbar Items for Larger Screens */}
+        </Toolbar>
+      </AppBar>
+      {isMobile ? (
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+          {/* Drawer Content */}
+        </Drawer>
+      ) : (
+        <div>
         </div>
-      </div>
-      <div className="nav-toggle" onClick={toggleNav}>
-        <div className={`burger ${isNavOpen ? "toggle" : ""}`} />
-      </div>
-    </nav>
+      )}
+    </div>
   );
 };
 
